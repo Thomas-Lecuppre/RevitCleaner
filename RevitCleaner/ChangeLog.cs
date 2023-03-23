3f6870d90260ab7600556904f5c6dc21bef26ba7
Thomas-LCP <71729025+Thomas-Lecuppre@users.noreply.github.com>
2023-03-24 00:38:15 +0100

    Ajout du système de mise à jour automatique

---
+++ RevitCleaner/ChangeLog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitCleaner
+{
+    public class ChangeLog
+    {
+        public enum ChangeLogType { Update = 1, Add = 2,  Remove = 3 }
+        public ChangeLogType Type { get; set; }
+        public string Change { get; set; }
+    }
+}
--- RevitCleaner/MainWindow.xaml.cs
+++ RevitCleaner/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Reflection;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading;
@@ -32,9 +33,6 @@ namespace RevitCleaner
         {
             this.InitializeComponent();
 
-            // Initialisation des vues modèle.
-            FilesListPage.MainWindowView = this;
-
             // Mise à jour de la barre de titre est de l'entête.
             PackageVersion pkgV = Package.Current.Id.Version;
 
@@ -42,6 +40,46 @@ namespace RevitCleaner
             ExtendsContentIntoTitleBar = true;
 
             SetTitleBar(TitleBar);
+
+            // Vérification des mises à jour.
+            CheckUpdateAsync();
+        }
+
+        private async void CheckUpdateAsync()
+        {
+            WebClient client = new();
+            Stream stream = client.OpenRead("https://update.thomas-lecuppre.fr/revitcleaner.txt");
+            StreamReader reader = new StreamReader(stream);
+
+            string updateInfo = await reader.ReadToEndAsync();
+            string[] content = updateInfo.Split('\n');
+
+            var newVersion = new Version(content[0]);
+            Package package = Package.Current;
+            PackageVersion packageVersion = package.Id.Version;
+            var currentVersion = new Version(string.Format("{0}.{1}.{2}.{3}", packageVersion.Major, packageVersion.Minor, packageVersion.Build, packageVersion.Revision));
+
+            //compare package versions
+            if (newVersion.CompareTo(currentVersion) > 0)
+            {
+                UpdatePage up = new UpdatePage();
+                up.MainWindowView = this;
+                up.InfoUpdate = $"Mettre à jour Revit Cleaner {currentVersion} vers {newVersion} ?";
+                up.ShowChangeLog(content.Skip(1).ToList());
+                FilesListPage.Content = up;
+            }
+            else 
+            { 
+                ShowMainPage(); 
+            }
+            
+        }
+
+        public void ShowMainPage()
+        {
+            MainPage mp = new MainPage();
+            mp.MainWindowView = this;
+            FilesListPage.Content = mp;
         }
 
     }
